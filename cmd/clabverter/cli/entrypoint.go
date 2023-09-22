c7bffa09e282b3a6e5f4dfc9cf402365cb564177
Roman Dodin <dodin.roman@gmail.com>
2023-09-22 12:49:46 +0300

    shorter line

--- cmd/clabverter/cli/entrypoint.go
+++ cmd/clabverter/cli/entrypoint.go
@@ -27,8 +27,9 @@ func Entrypoint() *cli.App {
 		Usage:   "run clabernetes clabverter -- clab to clabernetes manifest(s) converter",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name:     topologyFile,
-				Usage:    "set the topology file to parse. When not set, clabverter will look for a file named '*.clab.y*ml'",
+				Name: topologyFile,
+				Usage: `set the topology file to parse.
+If not set, clabverter will look for a file named '*.clab.y*ml'`,
 				Required: false,
 				Value:    "",
 			},
