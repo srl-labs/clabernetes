abe21646f832f95fc675e8cbe754635c2b387d1d
Carl Montanari <carl.r.montanari@gmail.com>
2023-12-11 17:28:42 -0800

    test: basic e2e test to not break clabverter more :)

---
+++ e2e/clabverter/clabverter_basic_test.go
@@ -0,0 +1,80 @@
+package clabverter_test
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	clabernetesclabverter "github.com/srl-labs/clabernetes/clabverter"
+	clabernetestesthelper "github.com/srl-labs/clabernetes/testhelper"
+	clabernetestesthelpersuite "github.com/srl-labs/clabernetes/testhelper/suite"
+)
+
+func TestMain(m *testing.M) {
+	clabernetestesthelper.Flags()
+
+	os.Exit(m.Run())
+}
+
+func TestClabverterBasic(t *testing.T) {
+	t.Parallel()
+
+	testName := "clabverter-basic"
+
+	namespace := clabernetestesthelper.NewTestNamespace(testName)
+
+	c := clabernetesclabverter.MustNewClabverter(
+		"test-fixtures/basic_clab.yaml",
+		"test-fixtures",
+		namespace,
+		"",
+		false,
+		false,
+		false,
+	)
+
+	err := c.Clabvert()
+	if err != nil {
+		t.Fatalf("failed running clabversion, err: %s", err)
+	}
+
+	// rename the generated topo so we can use the e2e runner thingy
+	err = os.Rename("test-fixtures/clabverter-basic.yaml", "test-fixtures/10-apply.yaml")
+	if err != nil {
+		t.Fatalf("failed renaming clabverted topology, err: %s", err)
+	}
+
+	defer func() {
+		err = os.Remove("test-fixtures/10-apply.yaml")
+		if err != nil {
+			t.Errorf("failed cleaning up clabverted topology file, err: %s", err)
+		}
+	}()
+
+	steps := clabernetestesthelpersuite.Steps{
+		{
+			Index:       10,
+			Description: "Create a simple containerlab topology from clabverted output",
+			AssertObjects: map[string][]clabernetestesthelpersuite.AssertObject{
+				"topology": {
+					{
+						Name: testName,
+						NormalizeFuncs: []func(t *testing.T, objectData []byte) []byte{
+							clabernetestesthelper.NormalizeTopology,
+						},
+					},
+				},
+				"service": {
+					{
+						Name: fmt.Sprintf("%s-srl1", testName),
+						NormalizeFuncs: []func(t *testing.T, objectData []byte) []byte{
+							clabernetestesthelper.NormalizeExposeService,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	clabernetestesthelpersuite.Run(t, steps, namespace)
+}
--- testhelper/kubernetes.go
+++ testhelper/kubernetes.go
@@ -3,14 +3,22 @@ package testhelper
 import (
 	"testing"
 
-	clabernetesutilkubernetes "github.com/srl-labs/clabernetes/util/kubernetes"
-
 	clabernetesutil "github.com/srl-labs/clabernetes/util"
+	clabernetesutilkubernetes "github.com/srl-labs/clabernetes/util/kubernetes"
 )
 
 const (
 	namespaceRandomPad = 8
 )
+
+// NewTestNamespace generates a namespace for a test.
+func NewTestNamespace(testName string) string {
+	return clabernetesutilkubernetes.SafeConcatNameKubernetes(
+		"e2e",
+		testName,
+		clabernetesutil.RandomString(namespaceRandomPad),
+	)
+}
 
 // NormalizeKubernetesObject does some janky regex replace to remove controller generated fields
 // we don't want to compare.
@@ -48,11 +56,45 @@ func NormalizeKubernetesObject(t *testing.T, object []byte) []byte {
 	return object
 }
 
-// NewTestNamespace generates a namespace for a test.
-func NewTestNamespace(testName string) string {
-	return clabernetesutilkubernetes.SafeConcatNameKubernetes(
-		"e2e",
-		testName,
-		clabernetesutil.RandomString(namespaceRandomPad),
+// NormalizeTopology normalizes a clabernetes topology cr by removing fields that may change between
+// ci and local or other folks machines/clusters -- so we can compare results more easily.
+func NormalizeTopology(t *testing.T, objectData []byte) []byte {
+	t.Helper()
+
+	// unfortunately we need to remove the hash bits since any cluster may have no lb or get a
+	// different lb address assigned than what we have stored in golden file(s)
+	objectData = YQCommand(
+		t,
+		objectData,
+		"del(.status.reconcileHashes.exposedPorts)",
 	)
+	objectData = YQCommand(
+		t,
+		objectData,
+		"del(.status.exposedPorts[].loadBalancerAddress)",
+	)
+
+	return objectData
+}
+
+// NormalizeExposeService normalizes a service cr by removing fields that may change between ci and
+// local or other folks machines/clusters -- so we can compare results more easily.
+func NormalizeExposeService(t *testing.T, objectData []byte) []byte {
+	t.Helper()
+
+	// cluster ips obviously are going to be different all the time so we'll ignore them
+	objectData = YQCommand(t, objectData, "del(.spec.clusterIP)")
+	objectData = YQCommand(t, objectData, "del(.spec.clusterIPs)")
+
+	// remove node ports since they'll be random
+	objectData = YQCommand(t, objectData, "del(.spec.ports[].nodePort)")
+
+	// and the lb ip in status because of course that may be different depending on cluster
+	objectData = YQCommand(
+		t,
+		objectData,
+		".status.loadBalancer = {}",
+	)
+
+	return objectData
 }
